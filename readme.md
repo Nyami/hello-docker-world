3c7e854e45ba0d381db61df916642d6ae2102fd8
Douglas Cameron <nyami@hotmail.com>
2021-03-10 13:05:25 +0000

    Added simple app with docker support

---
+++ readme.md
@@ -0,0 +1,4 @@
+Simple Hello World console app with docker support.
+
+Build it with `docker build -t hello-docker-world .\src\App\`
+Run it with `docker run hello-docker-world`
---
+++ src/App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nyami.Hello.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
